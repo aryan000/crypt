9417214a2bce892df3fd381643597d0e9a9945f6
root <root@localhost.localdomain>
2016-02-07 15:09:48 +0530

    font issue

---
+++ include/sidemenu_admin.php
@@ -0,0 +1,24 @@
+<div id="sidebar2" class="sidebar">
+			<ul>
+<?php
+require_once("functions.php");
+
+
+	$page=pagelevel($_SESSION['username']);
+echo '<li>
+					<h2>Profile Menu </h2>
+				  <ul>
+					  <li><a href="profile.php">Profile</a></li>
+					  <li><a href="questions/'.$page.'">Check levels</a></li>
+					
+				  </ul>
+				</li>';
+
+
+
+?>
+
+			 
+			  
+			</ul>
+		</div>
