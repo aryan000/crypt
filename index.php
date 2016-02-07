a60a5aac431ede85d09a374a6a9de47a569e7dad
root <root@localhost.localdomain>
2016-02-07 13:17:38 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,116 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+
+
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<title>Cryptex , Phoenix 2016</title>
+
+<meta name="keywords" content="" />
+
+<meta name="Gestured" content="" />
+
+
+<style type="text/css">
+@font-face
+{
+font-family: extras;
+src: url('fonts/extra.otf');
+}
+#contents
+{
+
+	font-family: extras;
+	font-size:170%;
+	margin-left:20px;
+	margin-right: 60px;
+	min-height: 600px;
+
+}
+#num{
+
+font-family: arial;
+
+}
+</style>
+	
+	
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="background">
+		<div id="header">
+			<?php
+
+	include("include/header.php");
+
+	?>
+		</div>
+		<div id="body">
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+		<div>
+		
+		
+			<div>
+			<?php
+				include("include/sidemenu.php");
+			?>
+			<div id="contents" >
+			
+			<br/><br/>
+			<p style = "margin-left:20px;">
+			
+			<font color = "red">Phoenix'<span id="num">16</span> presents Cryptex !</font>
+			<br/><br/>
+			The mega online code breaking event!<br /><br/>
+			</p>
+			<p style="font-size:80%;margin-left:20px;">
+
+Cryptex, is an exquisitely crafted online code breaking challenge modelled on the lines of a treasure hunt. The event assesses the participants for their sharp wits, general knowledge, lateral thinking ability as well as the awareness about random happenings.  <br><br>
+A tad bit of common sense coupled with good observation, is what is takes you to enter the event that is bound to be an all time favorite among students all over the country, like always. <br><br>
+So, sit back, get ready for the action & a chance to get your hands on the coveted 15k cash prizes!!
+  </p><br/><p style="font-size:80%;margin-left:20px;">
+
+There are 3 lifelines that are available <strong> from level 6 till level 20 </strong>for your help <br/>
+1. Level-Skip<br/>
+2. First-Last<br/>
+3. Answer-Length<br/>
+<br/>
+Hints available @ <u><a href="https://www.facebook.com/phoenix.cryptex/app_202980683107053" target="_blank">Discussion Forum</a></u><br /><br>
+For More Info Please Visit <u><a href = "rules.php" >Rules</a></u> Page!<br>
+<font color = "red">Do not press enter to submit your answer, simply click on submit!</font></br></br>
+
+<!--Play on the go with our android app, <a href="https://play.google.com/store/apps/details?id=com.csi.cryptextphoenix13" target="_blank">Click Here</a>!<br/><br/>-->
+
+ </p>
+
+			
+			
+				<br/>
+					
+					
+				</div>
+				</div>
+		</div>
+		</div>
+		</div>
+		
+		<?php include("include/footer.php"); ?>
+	</div>
+</body>
+</html>
